f1062c7d38e6900e4d2f483b61ad7921c847e5c4
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-04-17 11:18:34 +0700

    penutupan

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,15 +1,6 @@
 
-
-//step 5
-for (int x = low; x<= high; x++) {
-    arr[x] = B{x};
-}
-
-
-void output(){
-    cout << "\nData setelah diurutkan (Mergesort): ";
-    for (int i = 0; i < n; i++){
-        cout << arr[i] << "  ";
-    }
-    cout << endl;
+int main() {
+    input();
+    mergesort(0, n - 1);
+    output();
 }
