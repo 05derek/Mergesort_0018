29ae22b35f54492f6c21a9f519835ea8077e4de4
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-04-17 10:34:26 +0700

    MEmasukkan elemen array

---
+++ mergesort.cpp
@@ -0,0 +1,11 @@
+
+cout << "\n========================================" ;
+cout << "\nInputkan isi element Array";
+cout << "\n========================================" << endl;
+
+for (int i = 0; i < n; i++){
+    cout << "Masukkan Array index ke - " << i << ":";
+    cin >> arr[i];
+}
+
+
