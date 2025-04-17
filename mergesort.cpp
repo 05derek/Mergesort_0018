91e2ac6e4e105f365f7a17a016bfaa5fa526e80d
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-04-17 10:46:44 +0700

    step 2 & 3

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,6 +1,7 @@
 
-void MergeSort(int low, int High) {
-    if (low >= high) { //step 1
-        return ;   //step 1.a
-    }
-}
+int mid = (low + high)/2;  //step2
+
+//step 3
+//fungsi rekursi - memanggil diri sendiri
+mergesort(low, mid); //step 3.a
+mergesort (mid + 1, high);  //step 3.b
